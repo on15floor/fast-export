5bb219c331a353630e60ee7f920c913330dc3fd3
Aleksandr Anisimov <a.e.anisimov@nexters.com>
2022-02-25 17:27:07 +0200

    Param bigger_than now optional.

--- plugins/list_binary_file_extensions/__init__.py
+++ plugins/list_binary_file_extensions/__init__.py
@@ -6,7 +6,7 @@ class Filter:
     def __init__(self, args):
         args = args.split(',')
         self.file = args[0]
-        self.bigger_then = int(args[1])
+        self.bigger_than = 1024 if len(args) == 1 else int(args[1])
         self.extensions = set()
     
     def __del__(self):
@@ -18,7 +18,7 @@ class Filter:
         file_ctx = file_data['file_ctx']
         if file_ctx.isbinary() and \
                 not file_ctx.islink() and \
-                (file_ctx.size() > self.bigger_then):
+                (file_ctx.size() > self.bigger_than):
             file_name = file_data['filename'].split('/')[-1]
             ex = file_name.split('.')
             if len(ex) == 1:
