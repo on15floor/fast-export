a13ac9c13aa7bb041934ab402fdfb38ab1172a85
Aleksandr Anisimov <a.e.anisimov@nexters.com>
2022-02-28 15:33:02 +0200

    Fixed bug when binary file name and directory name coincided.

--- plugins/list_binary_file_extensions/__init__.py
+++ plugins/list_binary_file_extensions/__init__.py
@@ -7,22 +7,44 @@ class Filter:
         args = args.split(',')
         self.file = args[0]
         self.bigger_than = 1024 if len(args) == 1 else int(args[1])
-        self.extensions = set()
+        
+        self.text_extensions = set()
+        self.binary_files = set()
+        self.directories = set()
     
     def __del__(self):
         with open(self.file, 'w') as f:
-            for ex in sorted(self.extensions):
-                f.write(ex + '\n')
+            for el in sorted(set(self.finalizing_data())):
+                f.write(el + '\n')
     
     def file_data_filter(self, file_data):
+        self.directories.update(self.get_folders(file_data['filename']))
+
         file_ctx = file_data['file_ctx']
-        if file_ctx.isbinary() and \
-                not file_ctx.islink() and \
-                (file_ctx.size() > self.bigger_than):
-            file_name = file_data['filename'].split('/')[-1]
-            ex = file_name.split('.')
-            if len(ex) == 1:
-                ex = file_name
+        if not file_ctx.islink():
+            if file_ctx.isbinary():
+                if file_ctx.size > self.bigger_than:
+                    file_path = file_data['filename']
+                    self.binary_files.add(file_path)
             else:
-                ex = '*.' + ex[-1]
-            self.extensions.add(ex)
+                file_ex = self.get_file_extension(file_data['filename'])
+                self.text_extensions.add(file_ex)
+
+    @staticmethod
+    def get_file_extension(file_name):
+        file_name = file_name.split('/')[-1]
+        ext = file_name.split('.')
+        return file_name if len(ext) == 1 else '*.' + ext[-1]
+
+    @staticmethod
+    def get_folders(file_path):
+        return set(file_path.split('/')[:-1])
+    
+    def finalizing_data(self):
+        files_exeptions = set.union(self.text_extensions, self.directories)
+        for _file in self.binary_files:
+            ext = self.get_file_extension(_file)
+            if ext in files_exeptions:
+                yield '/' + _file
+            else:
+                yield ext
