4a8e3d7f56dbd240e05c547ce4eea3670a6a86da
Aleksandr Anisimov <a.e.anisimov@nexters.com>
2022-02-28 16:16:53 +0200

    Fixed case when directory could be named name.ext

--- plugins/list_binary_file_extensions/__init__.py
+++ plugins/list_binary_file_extensions/__init__.py
@@ -6,7 +6,7 @@ class Filter:
     def __init__(self, args):
         args = args.split(',')
         self.file = args[0]
-        self.bigger_than = 1024 if len(args) == 1 else int(args[1])
+        self.bigger_than = 0 if len(args) == 1 else int(args[1])
         
         self.text_extensions = set()
         self.binary_files = set()
@@ -18,33 +18,37 @@ class Filter:
                 f.write(el + '\n')
     
     def file_data_filter(self, file_data):
-        self.directories.update(self.get_folders(file_data['filename']))
+        file_name = file_data['filename']
+        self.directories.update(self.get_directories(file_name))
 
         file_ctx = file_data['file_ctx']
         if not file_ctx.islink():
             if file_ctx.isbinary():
                 if file_ctx.size > self.bigger_than:
-                    file_path = file_data['filename']
-                    self.binary_files.add(file_path)
+                    self.binary_files.add(file_name)
             else:
-                file_ex = self.get_file_extension(file_data['filename'])
+                file_ex = self.get_file_extension(file_name)
                 self.text_extensions.add(file_ex)
 
     @staticmethod
-    def get_file_extension(file_name):
-        file_name = file_name.split('/')[-1]
+    def get_file_extension(file_path):
+        file_name = file_path.split('/')[-1]
         ext = file_name.split('.')
         return file_name if len(ext) == 1 else '*.' + ext[-1]
+    
+    @staticmethod
+    def get_file_name(file_path):
+        return file_path.split('/')[-1]
 
     @staticmethod
-    def get_folders(file_path):
+    def get_directories(file_path):
         return set(file_path.split('/')[:-1])
     
     def finalizing_data(self):
-        files_exeptions = set.union(self.text_extensions, self.directories)
         for _file in self.binary_files:
             ext = self.get_file_extension(_file)
-            if ext in files_exeptions:
+            name = self.get_file_name(_file)
+            if ext in self.text_extensions or name in self.directories:
                 yield '/' + _file
             else:
                 yield ext
